33b6265d24135aa1206fd4cbad5ee7e69b6dbfa1
goldme2003 <goldme2003@gmail.com>
2020-07-02 16:38:51 +0800

    V0.91 complete mainpage leftside navibar menu using dumping methods TODO: 1. change ITTO item status according to chosen process 2. Modify menu and left navibar according to DB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {KsServiceService} from './service/ks-service.service';
+import {Ksmodel} from './ksmodel';
+import {pnModel} from './models/pnmodel';
+import {pgModel} from './models/pgmodel';
+import {ProcessgroupService} from './service/processgroup.service';
+import {ProcessService} from './service/process.service';
 
 
 @Component({
@@ -8,4 +14,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PMPWeb';
+
+  allksitems: Ksmodel[];
+  allpnitems: pnModel[];
+  allpgitems: pgModel[];
+
+  constructor(private pgService: ProcessgroupService, private ksService: KsServiceService, private pnService: ProcessService) { }
+
+
+  ngOnInit(): void {
+    this.listPg();
+    this.listKs();
+    this.showAllpns();
+  }
+
+  listPg(): void {
+    this.pgService.getAllPg()
+      .subscribe(allpgitems => this.allpgitems = allpgitems);
+  }
+
+  listKs(): void {
+    this.ksService.getAllKs()
+      .subscribe(allksitems => this.allksitems = allksitems);
+  }
+
+  showAllpns(): void {
+    this.pnService.getAllPn()
+      .subscribe(pnitems => this.allpnitems = pnitems);
+
+  }
 }
